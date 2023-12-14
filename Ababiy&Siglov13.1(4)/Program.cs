50ee073fa8c84779512e22a6188f96b170d3ea8d
F1ner <123871318+F1ner@users.noreply.github.com>
2023-12-14 15:44:03 +0200

    13.4(4)

---
+++ Ababiy&Siglov13.1(4)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ababiy_Siglov13._1_4_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tovar tovar = new Tovar();
+            tovar.Name=Console.ReadLine();
+            tovar.Price = int.Parse(Console.ReadLine());
+            tovar.Kvo = int.Parse(Console.ReadLine());
+
+            Tovar tovar1 = new Tovar();
+            tovar1.Name = Console.ReadLine();
+            tovar1.Price = int.Parse(Console.ReadLine());
+            tovar1.Kvo = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{tovar.ToString()}\n{tovar1.ToString()}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ababiy&Siglov13.1(4)/Tovar.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ababiy_Siglov13._1_4_
+{
+    internal class Tovar
+    {
+        string name;
+        int price, kvo;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int  Price
+        { 
+            get { return price; } 
+            set { price = (value >10 || value < 1) ? 0 : value; } 
+        }
+        public int Kvo
+        {
+            get { return kvo; }
+            set { kvo = (value > 10 || value < 0) ? 0 : value; }
+        }
+        public Tovar() { }
+        public Tovar(string name, int price, int kvo)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Kvo = kvo;
+        }
+        public override string ToString()
+        {
+            return $"Name-{name}\t Price-{price}\t Kvo-{kvo}";
+        }
+
+
+    }
+}
