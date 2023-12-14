85099a9a1926c3caed45d4a88582fcb59eee0862
F1ner <123871318+F1ner@users.noreply.github.com>
2023-12-14 14:52:24 +0200

    13.1

---
+++ Ababiy&Siglov13.1(1)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ababiy_Siglov13._1_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Иванов", 3);
+            Student student2 = new Student("", -7);
+            string f = Console.ReadLine();
+            int k= int.Parse(Console.ReadLine());
+            Student student1 = new Student(f,k);
+            
+            Console.WriteLine($"{student.ToString()}\n{student2.ToString()}\n{student1.ToString()}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ababiy&Siglov13.1(1)/Student.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ababiy_Siglov13._1_1_
+{
+    internal class Student
+    {
+        private string fam;
+        private int kurs;
+
+        public string Fam
+        {
+            
+
+            get 
+            {
+             return (fam!="" )?fam:"Неизвестно";    
+            }
+            set { fam = value.ToUpper(); }
+
+
+
+        }
+        public int Kurs { 
+            set 
+            {
+                if (value < 1 || value > 4)
+
+                    Console.WriteLine("Такого курса не бывает");
+
+                else
+
+                    kurs = value;
+
+                //kurs=(value<1||value>4)?0:value;
+
+            }
+            get { return kurs; }
+        }
+
+        public Student() { }
+        public Student(string fam, int kurs)
+        {
+            this.Fam = fam;
+            this.Kurs = kurs;
+        }
+        public override string ToString()
+        {
+            return $"Фамилия-{Fam}\tKurs-{Kurs}";
+        }
+    }
+}
