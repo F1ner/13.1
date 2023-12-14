025132841b171036c3e51f89d1ea5de9769d76b6
F1ner <123871318+F1ner@users.noreply.github.com>
2023-12-14 15:28:50 +0200

    13.1(3)

---
+++ Ababiy&Siglov13.1(3)/Computer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ababiy_Siglov13._1_3_
+{
+    internal class Computer
+    {
+        string model;
+        int ram, hdd;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public int Ram
+        {
+            get { return ram; }
+            set { ram = (value < 2 || value > 32) ? 0 : value; }
+        }
+        public int Hdd 
+        {
+        get { return hdd; }
+            set { hdd = (value < 200 || value > 2000) ? 0 : value; }
+        }
+        public Computer() { }
+        public Computer(string model,int ram,int hdd) 
+        {
+         this.Model = model;
+            this.Ram = ram;
+            this.Hdd = hdd;
+        }
+        public override string ToString() 
+        {
+            return $"Model-{model}\tRam-{ram}\tHdd-{hdd}";
+        }
+    }
+}
---
+++ Ababiy&Siglov13.1(3)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ababiy_Siglov13._1_3_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer computer = new Computer();
+            computer.Model=Console.ReadLine();
+            computer.Ram=int.Parse(Console.ReadLine());
+            computer.Hdd=int.Parse(Console.ReadLine());
+            Computer computer1 = new Computer();
+            computer1.Model = Console.ReadLine();
+            computer1.Ram = int.Parse(Console.ReadLine());
+            computer1.Hdd = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{computer.ToString()}\n{computer1.ToString()}");
+            Console.ReadKey();
+        }
+    }
+}
