042fef6b6feabab5366b3dedeef42e6ece85715b
F1ner <123871318+F1ner@users.noreply.github.com>
2023-12-14 15:14:51 +0200

    13.1(2)

---
+++ Ababiy&Siglov13.1(2)/Avto.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ababiy_Siglov13._1_2_
+{/// <summary>
+/// Класс описывающий авто
+/// </summary>
+    internal class Avto
+    {/// <summary>
+    /// поля
+    /// </summary>
+        string brand, color;
+        int skor;
+       /// <summary>
+       /// конструктор по умолчанию
+       /// </summary>
+        public Avto() { }
+        /// <summary>
+        /// конструктор с 3мя параметрами
+        /// </summary>
+        /// <param name="brand">поле описывающее марку машины</param>
+        /// <param name="color">поле описывающее цвет машины</param>
+        /// <param name="skor">поле описывающее скорость машины</param>
+        public Avto(string brand,string color ,int skor) 
+        {
+         this.Brand = brand;
+            this.Color = color;
+            this.Skor = skor;
+        }
+        /// <summary>
+        /// свойства поля описывающего марку машины
+        /// </summary>
+        public string Brand
+        { get { return brand; } set {  brand = value; } }
+        /// <summary>
+        /// свойства поля описывабщего цвет машины
+        /// </summary>
+       public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+        /// <summary>
+        /// свойства поля описывающего скорость машины
+        /// </summary>
+        public int Skor { get {  return skor; } set { skor = (value < 20 || value > 120) ? 0 :value; } }
+        /// <summary>
+        /// Метод вывода полей
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString() 
+        {
+            return $"Brand-{brand}\tColor-{color}\tSkor-{skor}";
+        }
+    }
+}
---
+++ Ababiy&Siglov13.1(2)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ababiy_Siglov13._1_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Avto avto = new Avto("bmw","red",10);
+            Avto avto1 = new Avto("audi", "green", 45);
+            Console.WriteLine($"{ avto.ToString()}\n{avto1.ToString()}");
+            Console.ReadKey();
+
+        }
+    }
+}
